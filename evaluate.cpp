1fb9a071e1ccc1742c315c6ad1dfba6598568081
joshua <kjkq26993@gaia.eonet.ne.jp>
2014-07-10 07:02:54 +0900

    evaluate.cpp,evaluate.hファイル追加

---
+++ evaluate.cpp
@@ -0,0 +1,10 @@
+
+
+#include "gtest\gtest.h"
+#include "position.h"
+#include "evaluate.h"
+
+int evaluate(Position &pos)
+{
+    return 1;
+}
---
+++ evaluate.h
@@ -0,0 +1,5 @@
+#if !defined(EVALUATE_H_INCLUDE)
+#define EVALUATE_H_INCLUDE
+
+
+#endif
