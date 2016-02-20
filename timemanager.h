229ba73a243bf1a3ea2cc55fe50525c3d6286bfd
joshua <kjkq26993@gaia.eonet.ne.jp>
2016-02-20 16:40:17 +0900

    search.hのためにTimeMangerクラスを宣言だけした

---
+++ timemanager.h
@@ -0,0 +1,13 @@
+#if !defined(TIME_MANAGER_H_INCLUDE)
+#define TIME_MANAGER_H_INCLUDE
+//search.hのため仮にTimeManagerクラスを定義
+
+struct LimitsType;
+
+//クラスの皮だけ定義
+class TimeManager{
+public:
+
+private:
+};
+#endif
