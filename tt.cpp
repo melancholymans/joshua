db2e00391bdbe770a1c81dd7f45c5701d5d4e166
joshua <kjkq26993@gaia.eonet.ne.jp>
2016-02-27 16:55:41 +0900

    Optionのためにtt.h,tt.cppを仮に作成

---
+++ tt.cpp
@@ -0,0 +1,6 @@
+#include "types.h"
+#include "tt.h"
+
+//Global TranspositionTable
+TranspositionTable TT;
+
--- tt.h
+++ tt.h
@@ -1,3 +1,11 @@
-class TranspositionTable{
+#if !defined(TT_H_INCLUDE)
+#define TT_H_INCLUDE
 
+class TranspositionTable {
+public:
+	void set_size(size_t mb_size);
+	void clear();
 };
+
+extern TranspositionTable TT;
+#endif
